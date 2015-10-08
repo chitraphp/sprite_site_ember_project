5bb8625b14cb480b461559108d97a4209c846819
ccmxy <windrainicefire@gmail.com>
2015-10-07 17:42:21 -0700

    added moment and google maps dependancies

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,15 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'sprite-site',
     environment: environment,
-    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    contentSecurityPolicy: {
+      'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com",
+      'default-src': "'none'",
+      'script-src': "'self' 'unsafe-eval' https://*.firebaseio.com https://*.googleapis.com https://*.gstatic.com",
+      'font-src': "'self' https://*.gstatic.com",
+      'img-src': "'self' *",
+      'style-src': "'self' 'unsafe-inline' https://*.googleapis.com",
+      'frame-src': "'self' https://*.firebaseio.com"
+      },
     firebase: 'https://fairy.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,9 +1,7 @@
-/* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
   });
 
   // Use `app.import` to add additional libraries to the generated
@@ -18,6 +16,10 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+
+
+  app.import('bower_components/moment/moment.js')
+  app.import('vendor/gmaps.js');
 
   return app.toTree();
 };
